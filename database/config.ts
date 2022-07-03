c95d788769e65bb1877eff380e29ef46d4f66d67
art0254 <nana.ort@gmail.com>
2022-07-04 06:35:15 +0700

    init

--- database/config.ts
+++
@@ -1,9 +0,0 @@
-import process from "process"
-
-export const config = {
-  DB_HOST: process.env.DB_SERVER_NAME,
-  DB_PORT: Number(process.env.DB_PORT),
-  DB_USERNAME: process.env.DB_USERNAME,
-  DB_PASSWORD: process.env.DB_PASSWORD,
-  DB_NAME: process.env.DB_NAME,
-}
